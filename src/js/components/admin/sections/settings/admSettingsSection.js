78ed3a50eeb89dd2e13e9e9828d3197800222e19
Alejandro Diaz <aldiazve@gmail.com>
2018-09-26 09:44:41 -0500

    machine swap

--- src/js/components/admin/sections/settings/admSettingsSection.js
+++ src/js/components/admin/sections/settings/admSettingsSection.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 //Styled components
-import styled, {ThemeProvider} from 'styled-components';
+import styled from 'styled-components';
 //Router
 //import { Switch, Route, Redirect} from "react-router-dom";
 //Customs components  
@@ -13,8 +13,6 @@ import AvailableActionsDiv from '../../../../utilities/availableActionsDiv.js';
 import Button from '../../../../utilities/button.js';
 //Custom Constants
 import * as Constants from '../../../../../constants.js';
-
-const theme = Constants.adminInnerSectionsTheme;
 
 //Section container
 const SectionContainer = styled('div')`
@@ -28,36 +26,59 @@ const SectionContainer = styled('div')`
   }
 `;
 
+const WhiteListContainer = styled('div')`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  min-height: 45px;
+  max-height: 180px;
+  overflow-x: auto;
+`;
+
+const WhiteListItem = styled('div')`
+  display: flex;
+  flex-direction: row;
+  height: 35px;
+  padding: 10px 5px;
+`
+
 class AdmSettingsSection extends React.Component {
-  state = {previousDocument: this.props.previousDocument};
-
-
 
   onWhiteListChange = (e) => {
-    console.log(e)
+    this.normalizeTextAreaContent(e.target.value);
   };
+
+  normalizeTextAreaContent = (textareaContent) => {
+    const emailList = textareaContent.split(/,| |\n/);
+    this.setState({whiteList: emailList});
+  }
 
   render() {
       console.log(this.props.hack)
     return (
-      <ThemeProvider theme={theme}>
-        <SectionContainer>
-          <h2>{this.props.hack.name}'s Settings</h2>
-          <Separator primary/>
-          <h3><label htmlFor='whiteList'>White List</label></h3>
-          <p>The white list is an email list that the defines which users are allow to register and participate in a hack (like a participants list). Please introduce the list of emails separated by commas (,).</p>
-          <textarea id='whiteList' placeholder='participant@email.com, participant@email.com, participant@email.com, participant@email.com...' defaultValue={this.props.hack.whiteList.length > 0 ? 'lala' : 'lele'} onChange={this.onWhiteListChange}></textarea>
-          <AvailableActionsDiv>
-            <Button 
-              primary
-              width='150px' 
-              margin='0 0 0 15px'
-              onClick={this.props.onSaveSettings}>
-              Save
-            </Button>
-          </AvailableActionsDiv>
-        </SectionContainer>
-      </ThemeProvider>
+      <SectionContainer>
+        <h2>{this.props.hack.name}'s Settings</h2>
+        <Separator primary/>
+        <h3><label htmlFor='whiteList'>White List</label></h3>
+        <p>The white list is an email list that the defines which users are allow to register and participate in a hack (like a participants list). Please introduce the list of emails separated by commas (,).</p>
+        <WhiteListContainer>
+          {this.state && this.state.whiteList.map((item, index) => {
+            return <WhiteListItem key={index}>
+              {item}
+            </WhiteListItem>
+          })}
+        </WhiteListContainer>
+        <textarea id='whiteList' placeholder='participant@email.com, participant@email.com, participant@email.com, participant@email.com...' onChange={this.onWhiteListChange}></textarea>
+        <AvailableActionsDiv>
+          <Button 
+            primary
+            width='150px' 
+            margin='0 0 0 15px'
+            onClick={this.props.onSaveSettings}>
+            Save
+          </Button>
+        </AvailableActionsDiv>
+      </SectionContainer>
     );
   }
 }
