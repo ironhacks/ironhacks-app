afac22760b473ce92705077b1c853b4ceee9c382
Alejandro Diaz Vecchio <aldiazve@gmail.com>
2019-04-01 05:46:32 -0500

    added homepage rank

---
+++ src/js/components/landing/priorHacksData.js
@@ -0,0 +1,202 @@
+const PriorHacksData = [
+	{
+		"name": "Purdue Unal 2017",
+		"description": "UNAL proved to be a strong strategic partner once again with students participating in the recent Purdue IronHacks competition that challenged students to create an app that helped university students living in the area find safe and affordable housing.",
+		"ranks": [
+			{
+				"name": "José Fernando Morantes Florez",
+				"place": "BEST SOLUTION",
+				"contact": "jfmorantesf@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "J",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "N",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "G",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "D",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "R",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "W",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "Y",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "U",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "Z",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+		],
+	},
+	{
+		"name": "Purdue Unal 2018",
+		"description": "While Professor Brunswicker was unable to attend in-person this year to the hack, Purdue Ironhacks has undoubtedly made its impact on the world.",
+		"ranks": [
+			{
+				"name": "José Fernando Morantes Florez",
+				"place": "BEST SOLUTION",
+				"contact": "jfmorantesf@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "J",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "N",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "G",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "D",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "R",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "W",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "Y",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "U",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "Z",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+		],
+	},
+	{
+		"name": "Purdue Unal 2019",
+		"description": "UNAL proved to be a strong strategic partner once again with students participating in the recent Purdue IronHacks competition that challenged students to create an app that helped university students living in the area find safe and affordable housing. While Professor Brunswicker was unable to attend in-person this year to the hack, Purdue Ironhacks has undoubtedly made its impact on the world.",
+		"ranks": [
+			{
+				"name": "José Fernando Morantes Florez",
+				"place": "BEST SOLUTION",
+				"contact": "jfmorantesf@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "J",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "N",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "G",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "D",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "R",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "W",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "Y",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "U",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+			{
+				"name": "Z",
+				"place": "THIRD PLACE",
+				"contact": "jjpulidos@unal.edu.co",
+				"country": "COL",
+			},
+		],
+	},
+]
+
+export default PriorHacksData;
