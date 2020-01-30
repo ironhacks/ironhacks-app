e8e813932100c7138dd494e40fff06a31d2a863d
Matt Harris <mjharris2407@gmail.com>
2020-01-30 09:24:24 -0500

    gitflow-feature-stash: update-dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { unregister } from './registerServiceWorker';
-import IronHacksApp from './ironhackApp.js';
-import 'bootstrap/dist/css/bootstrap-reboot.min.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import { unregister } from './serviceWorker';
+import App from './App.js';
+import './static/bootstrap-reboot.css';
+import './static/bootstrap-grid.css';
 import './main.css';
 
 ReactDOM.render((
