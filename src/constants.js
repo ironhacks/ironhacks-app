fd63a198c9a99e019b236520a89b8fe5519e1e27
Matt Harris <m@ttharr.is>
2020-01-30 01:27:52 -0500

    fixes file permissions and updates changes from dropbox files

--- src/constants.js
+++ src/constants.js
@@ -98,7 +98,7 @@ export const Error404Theme = {
   backgroundColor: "white",
   textColor: "black"
 }
-//Proyect Editor
+//Project Editor
 export const cloudFunctionsDevEndPoint = 'localhost:5000';
 export const cloudFunctionsProdEndPoint = 'https://us-central1-the-ironhacks-platform-dev.cloudfunctions.net';
 
