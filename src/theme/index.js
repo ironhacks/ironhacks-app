4ec26c1315bd6dc9637fab845b5caffd56b8908c
Matt Harris <mjharris2407@gmail.com>
2020-05-18 01:20:07 -0400

    adds theme "themes" module

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,14 @@
 import * as Units from './lib/units.js';
 import * as Colors from './lib/colors.js';
 import * as Styles from './lib/styles.js';
+import { Themes } from './lib/themes.js';
+
 
 const Theme = {
   UNITS: Units,
   COLORS: Colors,
   STYLES: Styles,
+  THEMES: Themes,
 };
 
 export {Theme};
