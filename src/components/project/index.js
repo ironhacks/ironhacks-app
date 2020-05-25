1417a6ef1fd59d9262b4ff614659b762145870dc
Matt Harris <mjharris2407@gmail.com>
2020-05-25 00:04:26 -0400

    projects components and view

---
+++ src/components/project/index.js
@@ -0,0 +1,12 @@
+import { IndexHtmlTemplate } from './templates/index-html.js';
+import { ScriptJsTemplate } from './templates/script-js.js';
+import { StyleCssTemplate } from './templates/style-css.js';
+
+
+const TemplateFiles = {
+  IndexHtmlTemplate,
+  ScriptJsTemplate,
+  StyleCssTemplate,
+}
+
+export { TemplateFiles }
