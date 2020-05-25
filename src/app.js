92330a122576b000df225d88862409fae4de331f
Matt Harris <mjharris2407@gmail.com>
2020-05-25 00:07:59 -0400

    updates main app and index

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Footer } from './views/layouts/sections/footer';
-import Reactotron from 'reactotron-react-js';
 import styled from 'styled-components';
 import { Loader } from './components/loader';
-import { AppRoutes } from './config/routes';
 // import { Loader, LoaderContainer } from './components/loader';
+// import { AppRoutes } from './config/routes';
 import {CookiesProvider} from 'react-cookie';
 import {
   Switch,
   Route,
-  BrowserRouter as Router,
   Redirect,
-  Link,
  } from 'react-router-dom';
 import Log from './util/log.js';
+
+import NewHack from './components/hacks/new-hack';
+import ProjectEditor from  './components/projectEditor/projectEditor.js';
+import ProjectPreview from './components/projectEditor/projectPreview.js';
+import QuizForm from './components/quizzes/quizForm.js';
+import Quizzes from './components/quizzes/quizzes.js';
+
 import { PageNotFound } from './views/default/404';
 import Header   from './views/layouts/sections/header';
 import HomePage from './views/home';
@@ -25,46 +29,43 @@ import ExamplesPage from './views/examples';
 import Admin from './views/admin';
 import AdminDashboard from './views/admin/adminDashboard';
 import Forum from './views/forum/forum.js';
-import hackSelector from './components/hacks/hack-selector';
-import NewHack from './components/hacks/new-hack';
+import { HackSelectPage, HackPage } from './views/hacks';
 import NewThread from './views/forum/newThread.js';
-import ProjectEditor from  './components/projectEditor/projectEditor.js';
-import ProjectPreview from './components/projectEditor/projectPreview.js';
-import QuizForm from './components/quizzes/quizForm.js';
-import Quizzes from './components/quizzes/quizzes.js';
+import ProjectsPage from  './views/projects';
 import Task from './views/task';
 import Results from './views/results';
-import './theme/styles.css';
 
+import './theme/styles.css';
+import './assets/main.css';
 
 const LoaderContainer = styled('div')`
   width: 100vw;
   height: 100vh;
 `;
 
-const testUser = {
-  uid: 111,
-  displayName: 'developer',
-  name: 'developer',
-  isAdmin: true,
-  admin: true,
-  email: 'developer@develer.com',
-};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {},
-      // user: testUser,
+      user: {
+        isAdmin: false,
+      },
+      userIsAdmin: false,
+      userHackData: null,
       mustNavigate: false,
     };
-    this.firestore = window.firebase.firestore();
+    this._isMounted = false;
   }
 
-
    componentDidMount() {
+    this._isMounted = true;
+     this.firestore = window.firebase.firestore();
      this._isUserConnected();
+   }
+
+   componentWillUnmount() {
+    this._isMounted = false;
    }
 
    _setUser(_user) {
@@ -80,18 +81,20 @@ class App extends React.Component {
        if (prevState.user) {
          prevState.user.isAdmin = _admin;
        }
-
        prevState.mustNavigate = true;
        return prevState;
      });
    }
 
    _isAdmin() {
-    this._setUpdateAdmin(true);
     this.firestore.collection('admins')
       .doc(this.state.user.uid)
       .get()
       .then((doc) => {
+        console.log('is admin', doc);
+        if (doc.exists) {
+          console.log(true);
+        }
         this._setUpdateAdmin(true);
       })
       .catch((error) => {
@@ -99,24 +102,63 @@ class App extends React.Component {
       })
    }
 
+  async _getUserHackdata(userId){
+    let userHackData = await window.firebase.firestore()
+      .collection('users')
+      .doc(userId)
+      .get()
+      .then((user)=>{
+        console.log(user.data());
+        return user.data();
+      })
+
+    return userHackData;
+  }
+
+  _setUserHackdata(hackData){
+    localStorage.setItem('userHackData', JSON.stringify(hackData));
+
+    if (this._isMounted){
+      this.setState({
+        userHackData: hackData,
+      })
+    }
+  }
+
   _isUserConnected() {
-    this._setUpdateAdmin(true);
+    // this._setUpdateAdmin(true);
     this._setShouldNavigate(true);
+    console.log('%c is user connected', 'color:red;font-weight:bold;');
     window.firebase
       .auth()
       .onAuthStateChanged((user) => {
         if (user) {
           const names = user.displayName.split(' ');
-          user.profileLetters = names[0].slice(0, 1) + names[1].slice(0, 1);
-          this._setUser(user);
+          const _user = {
+            displayName: user.displayName,
+            email: user.email,
+            emailVerified: user.emailVerified,
+            isAdmin: user.isAdmin,
+            metadata: user.metadata,
+            phoneNumber: user.photoURL,
+            photoURL: user.photoURL,
+            profileLetters: names[0].slice(0, 1) + names[1].slice(0, 1),
+            provider: user.providerData,
+            user: user.isAnonymous,
+            userId: user.uid,
+            uid: user.uid,
+          }
+          console.log('%c onAuthStateChanged', 'color:red;font-weight:bold;', _user);
+          this._setUser(_user);
           this._isAdmin();
+          let hackData = this._getUserHackdata(_user.uid);
+          this._setUserHackdata(hackData);
         } else {
-          this._setUser(false);
+          this._setUser({});
           this._setShouldNavigate(true);
         }
     });
   }
-
 
   render() {
       if(!this.state.mustNavigate){
@@ -132,38 +174,49 @@ class App extends React.Component {
        return (
          <CookiesProvider>
           <div className='App'>
-
            <Switch>
               <Route exact path='/' render={() => null}/>
-              <Route path='/login' component={Login}/>
+              <Route path='/login' render={() => null}/>
+              <Route exact path='/404' render={() => null}/>
+              <Route exact path='/projectEditor/:proyectName/preview' render={() => null}/>
 
               {!this.state.user && <Redirect to='/'/>}
               <Route render={(props) => (<Header user={this.state.user} {...props}/>)}/>
-
             </Switch>
 
             <Switch>
-
               <Route path='/login' component={Login}/>
-              <Route path='/select-hack' render={(props) => (<hackSelector user={this.state.user} {...props}/>)}/>
+
               <Route path='/profile' render={(props) => (<UserProfile user={this.state.user} {...props}/>)}/>
+              <Route path='/profile/:userId' render={(props) => (<UserProfile user={this.state.user} {...props}/>)}/>
+
               <Route exact path='/forum' render={(props) => (<Forum user={this.state.user} {...props}/>)}/>
               <Route exact path='/forum/new' render={(props) => (<NewThread user={this.state.user} {...props}/>)}/>
               <Route path='/forum/thread/:threadId' render={(props) => (<ThreadViewWithRouter user={this.state.user} {...props}/>)}/>
+
               <Route exact path='/admin/newHack' component={NewHack}/>
               <Route path='/admin/dashboard/:hackId' component={AdminDashboard}/>
               <Route path='/admin' component={Admin}/>
+
+              <Route path='/examples' render={(props) => (<ExamplesPage user={this.state.user} {...props}/>)}/>
               <Route path='/tutorial' render={(props) => (<TutorialScreen user={this.state.user} {...props}/>)}/>
               <Route path='/task' render={(props) => (<Task user={this.state.user} {...props}/>)}/>
-              <Route path='/examples' render={(props) => (<ExamplesPage user={this.state.user} {...props}/>)}/>
+
               <Route exact path='/quizzes' component={Quizzes}/>
               <Route path='/quizzes/:quizName' render={(props) => (<QuizForm user={this.state.user} {...props}/>)}/>
+
               <Route path='/results' render={(props) => (<Results user={this.state.user} {...props}/>)}/>
+
+              <Route exact path='/hacks' render={(props) => (<HackSelectPage user={this.state.user} {...props}/>)}/>
+              <Route path='/hacks/:hackId' render={(props) => (<HackPage user={this.state.user} {...props}/>)}/>
+              <Route path='/hack-settings/:hackId' component={AdminDashboard}/>
+
+              <Route exact path='/projects' render={(props) => (<ProjectsPage user={this.state.user} {...props}/>)}/>
               <Route exact path='/projectEditor/:proyectName' render={(props) => (<ProjectEditor user={this.state.user} {...props}/>)}/>
               <Route exact path='/projectEditor/:proyectName/preview' render={(props) => (<ProjectPreview user={this.state.user} {...props}/>)}/>
               <Route exact path='/404' component={PageNotFound}/>
 
-              {this.state.user.admin && <Redirect to='/admin'/>}
+              {this.state.user.isAdmin && <Redirect to='/admin'/>}
               <Route exact path='/' component={HomePage}/>
 
               {this.state.user && <Redirect to='/404'/>}
@@ -187,41 +240,41 @@ class App extends React.Component {
 }
 
 
-
-export function RouteConfigExample() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to='/tacos'>Tacos</Link>
-          </li>
-          <li>
-            <Link to='/sandwiches'>Sandwiches</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          {AppRoutes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route} />
-          ))}
-        </Switch>
-      </div>u
-    </Router>
-  );
-}
-
-
-// pass the sub-routes down to keep nesting
-function RouteWithSubRoutes(route) {
-  return (
-    <Route
-      path={route.path}
-      render={props => (
-        <route.component {...props} routes={route.routes} />
-      )}
-    />
-  );
-}
+//
+// export function RouteConfigExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to='/tacos'>Tacos</Link>
+//           </li>
+//           <li>
+//             <Link to='/sandwiches'>Sandwiches</Link>
+//           </li>
+//         </ul>
+//
+//         <Switch>
+//           {AppRoutes.map((route, i) => (
+//             <RouteWithSubRoutes key={i} {...route} />
+//           ))}
+//         </Switch>
+//       </div>u
+//     </Router>
+//   );
+// }
+//
+//
+// // pass the sub-routes down to keep nesting
+// function RouteWithSubRoutes(route) {
+//   return (
+//     <Route
+//       path={route.path}
+//       render={props => (
+//         <route.component {...props} routes={route.routes} />
+//       )}
+//     />
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,40 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {unregister} from './serviceWorker';
-// import Reactotron from 'reactotron-react-js';
-import Log from './util/log';
-import {fireApi} from './services/fire-api';
-import './ReactotronConfig'
 import App from './app';
+// import {fireApi} from './services/fire-api';
 
-if (process.env.NODE_ENV !== 'production' && process.env.TEST_HACK) {
-  const devHackData = {
-    ihdemo: {
-      forumId: 'Ey3ixM3mtmsHhXIPHVff',
-      hackId: 'eKZyDxD4TGIsvzGAHF6f',
-    },
-    ihplatformreview: {
-      hackId: 'mmHJrWzmx4rCyQ2YpJWL',
-      forumId: [
-        'UI0uDZcAyldOnftvl1Pr',
-        'UI0uDZcAyldOnftvl1Pr',
-      ],
-    },
-    purdueunal2019: {
-      hackId: 'mmHJrWzmx4rCyQ2YpJWL',
-      forumId: [
-        'qJmgIAFB6FtazeS66vJu',
-        '8JKHD71CFYS2SzI52UQ9',
-      ],
-    },
-  };
-
-  localStorage.setItem('debug','"development" ');
-  Log.info('app loaded');
-}
+// if (process.env.NODE_ENV !== 'production' && process.env.TEST_HACK) {
+  // const devHackData = {
+  //   ihdemo: {
+  //     forumId: 'Ey3ixM3mtmsHhXIPHVff',
+  //     hackId: 'eKZyDxD4TGIsvzGAHF6f',
+  //   },
+  //   ihplatformreview: {
+  //     hackId: 'mmHJrWzmx4rCyQ2YpJWL',
+  //     forumId: [
+  //       'UI0uDZcAyldOnftvl1Pr',
+  //       'UI0uDZcAyldOnftvl1Pr',
+  //     ],
+  //   },
+  //   purdueunal2019: {
+  //     hackId: 'mmHJrWzmx4rCyQ2YpJWL',
+  //     forumId: [
+  //       'qJmgIAFB6FtazeS66vJu',
+  //       '8JKHD71CFYS2SzI52UQ9',
+  //     ],
+  //   },
+  // };
+// }
 
 if (process.env.NODE_ENV !== 'production') {
   window.__DEV__ = true;
+  localStorage.setItem('debug','"development" ');
 } else {
   window.__DEV__ = false;
 }
