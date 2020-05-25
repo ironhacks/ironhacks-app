c8920c3668c5425237526ea8b8c747c2c1fad968
Matt Harris <mjharris2407@gmail.com>
2020-05-25 00:05:45 -0400

    updates footer removes styled components adds component stylesheet

--- src/views/layouts/sections/footer.js
+++ src/views/layouts/sections/footer.js
@@ -1,46 +1,42 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { Theme } from '../../../theme';
+import './assets/footer.css';
 
-const colors = Theme.COLORS;
-const styles = Theme.STYLES.FooterTheme;
-
-const FooterContainer = styled('div')`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-  height: ${(props) => props.theme.containerHeight};
-  width: 100%;
-  background-color: ${colors.mainBgColor} span {
-  font-size: 12px;
-
-    &:first-child {
-      text-align: left;
-    }
-
-    &:last-child {
-      line-height: 12px;
-      font-weight: 700;
-      text-align: right;
-    }
-  }
-`;
 
 class Footer extends React.Component {
   render() {
     return (
-      <ThemeProvider theme={styles}>
-        <FooterContainer>
-          <span>Version 2.0.1</span>
-          <span>
-            RESEARCH CENTER FOR OPEN DIGITAL INNOVATION | RCODI
-            <br />
-            All rigths reserved IronHacks&#169; 2019
-          </span>
-        </FooterContainer>
-      </ThemeProvider>
-    );
+      <footer className="footer site_footer">
+        <div
+          className={'footer_container'}
+          style={{
+            margin: '0 auto',
+            maxWidth: 'var(--page-width)',
+            paddingTop: '2rem'
+          }}>
+
+          <div
+            className="footer_block"
+            style={{
+              marginLeft: '3rem'
+            }}>
+            <span>Version 2.0.1</span>
+          </div>
+
+          <div
+            className='footer_block'
+            style={{
+              marginRight: '3rem'
+            }}>
+            <div>
+              RESEARCH CENTER FOR OPEN DIGITAL INNOVATION | RCODI
+            </div>
+            <div>
+              All rigths reserved IronHacks&#169; {new Date().getFullYear()}
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
   }
 }
 
