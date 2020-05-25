8b7e7039e8b291f5b24f23b23b10217119f86fa7
Matt Harris <mjharris2407@gmail.com>
2020-05-25 00:04:38 -0400

    adds button components

---
+++ src/components/buttons/button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+import { Theme } from "../../theme"
+const colors = Theme.COLORS
+const units = Theme.UNITS
+
+const Button = styled("button")`
+  width: ${props => (props.width ? props.width : "100%")};
+  height: ${props => (props.height ? props.height : "30px")};
+  margin: ${props => (props.margin ? props.margin : "0")};
+  background-color: ${props =>
+    props.primary ? colors.mainBgColor : "lightgray"};
+  border-radius: ${units.universalBorderRadius}l;
+  border: none;
+`
+
+export default Button
