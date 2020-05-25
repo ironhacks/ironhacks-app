b97a413fe35ccdbaf256a6ee0f523f134eb0b3e7
Matt Harris <mjharris2407@gmail.com>
2020-05-25 12:51:50 -0400

    updates hack  examples page to example section component

--- src/views/examples/index.js
+++ src/views/examples/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { withCookies } from 'react-cookie';
 import styled, { ThemeProvider } from 'styled-components';
-import Separator from '../../util/separator.js';
 import { Theme } from '../../theme';
 import {Loader} from '../../components/loader';
 import Examples from './lib/d3-examples';
@@ -34,42 +31,20 @@ const SectionContainer = styled('div')`
   }
 `;
 
-class UserProfile extends React.Component {
+class ExamplesPage extends React.Component {
   constructor(props) {
     super(props);
-    const { cookies } = props;
-
     this.state = {
-      currentHack: cookies.get('currentHack') || null,
-      forum: cookies.get('currentForum') || null,
-      startNewProjecNav: false,
-      startProjectEditorNav: false,
-      hackData: null,
-      user: this.props.user,
-    };
-
-    if (this.props.location.state) {
-      this.state.user = this.props.location.state.user;
+      loading: false,
     }
-
-    this.firestore = window.firebase.firestore();
   }
 
   componentDidMount() {
-    if (!this.state.user.isAdmin) {
-      this.getCurrentHackInfo();
-    }
+    // if (!this.state.user.isAdmin) {
+    //   this.getCurrentHackInfo();
+    // }
   }
 
-  goToProjectEditor = (index) => {
-    this.setState({
-      selectedProject: index,
-      navigateToProject: true,
-    });
-  };
-
-
-  onPhaseSelection = (phase) => {};
 
   render() {
     if (this.state.loading) {
@@ -82,47 +57,14 @@ class UserProfile extends React.Component {
       );
     }
 
-    if (this.state.navigateToProject === true) {
-      return (
-        <Redirect
-          push
-          to={`projectEditor/${
-            this.state.projects[this.state.selectedProject].name
-          }`}
-        />
-      );
-    }
-    if (this.state.navigateToCreatedProject === true) {
-      return (
-        <Redirect push to={`projectEditor/${this.state.newProjectName}`} />
-      );
-    }
     return (
       <ThemeProvider theme={styles}>
         <SectionContainer>
-          <div>
-            <h2 className='padding'>D3.js Examples</h2>
-            <h2 className='padding'>Projects</h2>
-            <span className='padding'>
-            Bellow you will find the current hack status. You can also manage
-            your projects from here.
-            </span>
-          </div>
           <Examples user={this.state.user}/>
-          <Separator primary className='padding' />
-
         </SectionContainer>
       </ThemeProvider>
     );
   }
 }
 
-// {this.state.hackData &&
-//           <TimeLine
-//             phases={this.state.hackData.phases}
-//             onClick={this.onPhaseSelection}
-//             currentPhase={this.state.currentPhase}
-//           />
-//         }
-
-export default withCookies(UserProfile);
+export default ExamplesPage;
