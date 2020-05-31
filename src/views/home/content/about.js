f65d98e4be8ab589aeb91f130dcd06ed79f4d964
Jia Lin Cheoh <jcheoh@purdue.edu>
2020-05-31 19:50:05 -0400

    messed around with showcase styling

--- src/views/home/content/about.js
+++ src/views/home/content/about.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import app1 from './images/screencapture-ironhacks-github-io-showcase-2018-spring-ialemusm-2020-05-25-02_43_33.png';
 
 export default class AboutSection extends React.Component {
   render() {
     return (
       <>
         <div>
+	  <h2>Winning App 4. Click Image to Access</h2>
+
+	<p>This is winning app 3. You can click on it.</p>
+
+	<img src={app1} alt="HTML tutorial"/>
           <p>
             IronHacks is an open data hacking program that combines experiential
             learning with real-world data-driven problem solving. During a
@@ -12,6 +18,8 @@ export default class AboutSection extends React.Component {
             novel and useful interactive visualizations and analytic "apps" that
             solve civic challenges.
           </p>
+	  
+
         </div>
         <div>
           <img src='' alt='' />
