b90aa769866683c46f17dd7f89a688c94bc216d2
Jia Lin Cheoh <jcheoh@purdue.edu>
2020-05-31 23:04:02 -0400

    looking decent version

--- src/views/home/content/about.js
+++ src/views/home/content/about.js
@@ -19,25 +19,36 @@ export default class AboutSection extends React.Component {
 	  </div>
 	  <div class="card__info">
           </div>
-          </div>
+         </div>
          <div class="card">
 	  <img src={app2} alt="HTML tutorial" class="card__image"/>
           <div class="card__content">
           <h2>This is winning app 2. You can click on it.</h2>
 	  </div>
+         <div class="card__info">
+          </div>
 	 </div>
+	 <div class="card">
+          <img src={app3} alt="HTML tutorial" class="card__image"/>
+          <div class="card__content">
 	  <h2>This is winning app 3. You can click on it.</h2>
-
-          <img src={app3} alt="HTML tutorial"/>
-
+          </div>
+          <div class="card__info">
+          </div>
+         </div>
+         <div class="card">
+          <img src={app4} alt="HTML tutorial" class="card__image"/>
+         <div class="card__content">
 	  <h2>This is winning app 4. You can click on it.</h2>
-
-          <img src={app4} alt="HTML tutorial"/>
-
-	  <h2>This is winning app 5. You can click on it.</h2>
+         </div>
+	  <div class="card__info">
+          </div>
+          </div>
+         <div class="card">
 
           <img src={app5} alt="HTML tutorial"/>
-
+          <h2>This is winning app 5. You can click on it.</h2>
+         </div>
           <h2>This is winning app 6. You can click on it.</h2>
 
           <img src={app6} alt="HTML tutorial"/>
