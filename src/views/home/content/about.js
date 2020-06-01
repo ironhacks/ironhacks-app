d3618e36919934ee3d3ebd8c3a827afecbdc5393
Jia Lin Cheoh <jcheoh@purdue.edu>
2020-06-01 00:38:08 -0400

    final touch up

--- src/views/home/content/about.js
+++ src/views/home/content/about.js
@@ -15,7 +15,7 @@ export default class AboutSection extends React.Component {
   	<div class="card">
           <img src={app1} alt="HTML tutorial" class="card__image"/>
           <div class="card__content">
-          <h3>Name | Year</h3>
+          <h3>Winner 2018</h3>
 	  <p>This is winning app 1. You can click on it. This app is so so so awesome. You know.</p>
 	  </div>
 	  <div class="card__info">
@@ -30,7 +30,7 @@ export default class AboutSection extends React.Component {
          <div class="card">
 	  <img src={app2} alt="HTML tutorial" class="card__image"/>
           <div class="card__content">
-          <h3>Name | Year</h3>
+          <h3>Winner 2018</h3>
           <p>This is winning app 2. You can click on it. This app is so so so awesome. You know.</p>
 	  </div>
          <div class="card__info">
@@ -45,7 +45,7 @@ export default class AboutSection extends React.Component {
 	 <div class="card">
           <img src={app3} alt="HTML tutorial" class="card__image"/>
           <div class="card__content">
-          <h3>Name | Year</h3>
+          <h3>Winner 2018</h3>
 	  <p>This is winning app 3. You can click on it. This app is so so so awesome. You know.</p>
           </div>
           <div class="card__info">
@@ -60,7 +60,7 @@ export default class AboutSection extends React.Component {
          <div class="card">
           <img src={app4} alt="HTML tutorial" class="card__image"/>
          <div class="card__content">
-          <h3>Name | Year</h3>
+          <h3>Winner 2019</h3>
 	  <p>This is winning app 4. You can click on it. This app is so so so awesome. You know.</p>
          </div>
 	  <div class="card__info">
@@ -75,7 +75,7 @@ export default class AboutSection extends React.Component {
          <div class="card">
           <img src={app5} alt="HTML tutorial" class="card__image"/>
          <div class="card__content">
-         <h3>Name | Year</h3>
+         <h3>Winner 2019</h3>
           <p>This is winning app 5. You can click on it. This app is so so so awesome. You know.</p>
          </div>
          <div class="card__info">
@@ -90,7 +90,7 @@ export default class AboutSection extends React.Component {
           <div class="card">
           <img src={app6} alt="HTML tutorial" class="card__image"/>
           <div class="card__content">
-          <h3>Name | Year</h3>
+          <h3>Winner 2019</h3>
           <p>This is winning app 6. You can click on it. This app is so so so awesome. You know.</p>
           </div> 
           <div class="card__info">
