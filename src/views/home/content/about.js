8e616812f392ea4214c87c0ea92aabb4b8e10a0b
Jia Lin Cheoh <jcheoh@purdue.edu>
2020-05-31 21:24:35 -0400

    included images of all apps

--- src/views/home/content/about.js
+++ src/views/home/content/about.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import app1 from './images/screencapture-ironhacks-github-io-showcase-2018-spring-ialemusm-2020-05-25-02_43_33.png';
-
+import app2 from './images/screencapture-ironhacks-github-io-showcase-2018-spring-juaccardenasgom-2020-05-25-02_42_01.png';
+import app3 from './images/screencapture-ironhacks-github-io-showcase-2018-spring-nmcasasr-2020-05-25-02_45_04.png';
+import app4 from './images/screencapture-ironhacks-github-io-showcase-2019-spring-crabaqueropi-2020-05-25-02_41_12.png';
+import app5 from './images/screencapture-ironhacks-github-io-showcase-2019-spring-difcortesgu-2020-05-25-02_40_28.png';
+import app6 from './images/screencapture-ironhacks-github-io-showcase-2019-spring-jdrodriguezrui-2020-05-25-02_37_56.png';
 export default class AboutSection extends React.Component {
   render() {
     return (
       <>
         <div>
-	  <h2>Winning App 4. Click Image to Access</h2>
 
-	<p>This is winning app 3. You can click on it.</p>
+	  <h2>This is winning app 1. You can click on it.</h2>
 
-	<img src={app1} alt="HTML tutorial"/>
-          <p>
-            IronHacks is an open data hacking program that combines experiential
-            learning with real-world data-driven problem solving. During a
-            5-stage virtual competition, students utilize open data to create
-            novel and useful interactive visualizations and analytic "apps" that
-            solve civic challenges.
-          </p>
-	  
+	  <img src={app1} alt="HTML tutorial"/>
+
+          <h2>This is winning app 2. You can click on it.</h2>
+
+          <img src={app2} alt="HTML tutorial"/>
+
+	  <h2>This is winning app 3. You can click on it.</h2>
+
+          <img src={app3} alt="HTML tutorial"/>
+
+	  <h2>This is winning app 4. You can click on it.</h2>
+
+          <img src={app4} alt="HTML tutorial"/>
+
+	  <h2>This is winning app 5. You can click on it.</h2>
+
+          <img src={app5} alt="HTML tutorial"/>
+
+          <h2>This is winning app 6. You can click on it.</h2>
+
+          <img src={app6} alt="HTML tutorial"/>
 
         </div>
         <div>
