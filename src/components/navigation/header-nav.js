f7783e21b087681298ac612d16dd813120edbcbf
Matt Harris <mjharris2407@gmail.com>
2020-07-03 17:17:36 -0400

    Updates page navigation components

---
+++ src/components/navigation/header-nav.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavContainerDiv } from './nav-container'
+import menuIcon from '../../assets/svg/menu-icon.svg'
+
+class HeaderNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showMenu: 'none',
+    };
+
+    this.navMenuRef = props.navMenuref
+  }
+
+  hideMenu = (event) => {
+    this.setState({
+      showMenu: 'none',
+    });
+  };
+
+  showMenu = () => {
+    if (this.state.showMenu === 'none') {
+      this.setState({ showMenu: 'flex' });
+    } else {
+      this.setState({ showMenu: 'none' });
+    }
+    ;
+  };
+
+  render() {
+    return (
+      <NavContainerDiv
+        display={this.state.showMenu}
+        innerRef={this.navMenuRef}
+      >
+
+        <button onClick={this.showMenu}>
+          <img src={menuIcon} alt='menu_icon' />
+        </button>
+
+        {/*<div className='links-container'>
+        </div>*/}
+      </NavContainerDiv>
+    )
+  }
+}
+export { HeaderNav }
