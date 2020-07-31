ec3fce3ac639525c77c18c49e81748fdf22f1a51
Matt Harris <mjharris2407@gmail.com>
2020-07-31 16:37:10 -0400

    Adds new list component

---
+++ src/components/list/lib/list.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function List({ items, listClass, itemClass }) {
+  return (
+    <ul className={`list ${listClass}`}>
+      {items.map((item, index)=>(
+        <li key={index}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+List.defaultProps = {
+  listClass: '',
+  itemClass: '',
+}
+
+export { List }
